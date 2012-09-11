6cf689940fb329514ec0b71b71c1c773400306e6
tomcz <github@tomczarniecki.com>
2012-09-11 16:11:14 -0700

    use resource configuration base class to remove boilerplate code

--- pico/src/main/java/example/pico/ResourceModule.java
+++ pico/src/main/java/example/pico/ResourceModule.java
@@ -1,36 +1,13 @@
 package example.pico;
 
-import com.google.common.collect.ImmutableList;
-import com.sun.jersey.spi.pico.container.servlet.BlankPicoConfiguration;
+import com.sun.jersey.spi.pico.container.servlet.ResourceConfiguration;
 import example.domain.resources.DocumentResource;
 import example.domain.resources.IndexResource;
 import example.domain.resources.StatusResource;
-import org.picocontainer.MutablePicoContainer;
 
-import java.util.List;
-import java.util.Set;
-
-public class ResourceModule extends BlankPicoConfiguration {
-
-    private final List<Class<?>> resourceClasses;
+public class ResourceModule extends ResourceConfiguration {
 
     public ResourceModule() {
-        this.resourceClasses = ImmutableList.<Class<?>>builder()
-                .add(StatusResource.class)
-                .add(IndexResource.class)
-                .add(DocumentResource.class)
-                .build();
-    }
-
-    @Override
-    public void registerResourceScope(MutablePicoContainer scope) {
-        for (Class<?> resourceClass : resourceClasses) {
-            scope.addComponent(resourceClass);
-        }
-    }
-
-    @Override
-    public void registerResources(Set<Class<?>> resources) {
-        resources.addAll(resourceClasses);
+        super(StatusResource.class, IndexResource.class, DocumentResource.class);
     }
 }
